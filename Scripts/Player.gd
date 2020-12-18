95ffda25d56847e1a124f7d617ae6aaa8c0f913f
Banana_Joestar <colassaltitan@comcast.net>
2020-12-18 18:19:53 -0500

    Provided a place holder image that will be used as the a player model. I also added some comments to clear up some confusion on what functions in code do. A lot of code typed in code right now is from referance to an online source since I am new to gdscript. I will make sure to have player movement code working soon.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,9 @@ export (int) var speed = 200
 
 var velocity = Vector2()
 
+var player
+
+# This is the logic behind the controls of the for the player.
 func getInput():
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
@@ -17,9 +20,16 @@ func getInput():
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
+# This calls the getInput function and provides the logic for the physics.
 func physicsProcess(_delta):
 	getInput()
 	velocity = move_and_slide(velocity)
+
+func _ready():
+
+	# This gets the image that is used for the player model.
+	playerModel = get_node("root/World/Player/[PH] PlayerModel")
+
 
 
 
