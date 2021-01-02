ac1a53437ed68bd89bbb9cd1521969076107551c
Luke <lakuch805@gmail.com>
2021-01-02 17:57:46 -0500

    Changed PlayerModel to PlayerSprite so that its less confusing, added playerSpriteIdle

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,17 +13,21 @@ export (int) var speed = 200
 
 var velocity = Vector2()
 
+var playerSpriteLeft : bool = false
+
 var player
 
-var playerModel : Sprite
+onready var playerSprite : AnimatedSprite = get_node("/root/World/Player/PlayerSprite")
 
 # This is the logic behind the controls of the for the player.
 func getInput():
 	velocity = Vector2()
 	if Input.is_action_pressed('PlayerRight'):
 		velocity.x += 1
+		playerSpriteLeft = false
 	if Input.is_action_pressed('PlayerLeft'):
 		velocity.x -= 1
+		playerSpriteLeft = true
 	if Input.is_action_pressed('PlayerDown'):
 		velocity.y += 1
 	if Input.is_action_pressed('PlayerUp'):
@@ -37,9 +41,11 @@ func _physics_process(_delta):
 	getInput()
 	velocity = move_and_slide(velocity)
 
+func _process(_delta):
+	playerSprite.flip_h = playerSpriteLeft;
+
 func _ready():
-	# This gets the Sprite in World Scene for player's model. 
-	playerModel = get_node("/root/World/Player/[PH] PlayerModel")
+	pass
 
 
 
